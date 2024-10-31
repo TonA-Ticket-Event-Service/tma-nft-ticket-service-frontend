b54265c97baa135a5f0502090b6fd0ec7c3d6988
vanvanich <panahno13@gmail.com>
2024-10-31 16:54:44 +0200

    remove redurant code

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,13 @@
-import { useState } from 'react'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-import WebApp from '@twa-dev/sdk'
 import {WalletConnector} from "./wallet-connector/WalletConnector.tsx";
 
 function App() {
-    const [count, setCount] = useState(0)
 
     return (
         <>
-            <div>
-                <a href="https://ton.org/dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="TWA logo" />
-                </a>
-                <a href="https://vitejs.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img src={viteLogo} className="logo react" alt="React logo" />
-                </a>
-            </div>
             <h1>TWA + Vite + React</h1>
             <WalletConnector/>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-            </div>
-            {/*  */}
-            <div className="card">
-                <button onClick={() => WebApp.showAlert(`Hello World! Current count is ${count}`)}>
-                    Show Alert
-                </button>
-            </div>
         </>
     )
 }
