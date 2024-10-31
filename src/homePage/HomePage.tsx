e467313152ed04bc0a383c8f115d9f356a82e514
vanvanich <panahno13@gmail.com>
2024-10-31 21:18:05 +0200

    add buttons for main page and styles for it

--- src/homePage/HomePage.tsx
+++ src/homePage/HomePage.tsx
@@ -1,8 +1,18 @@
+
 const HomePage = () => {
     return (
         <>
             <h1>NFT HOT EXAMPLE</h1>
             <span>You can choose</span>
+            <div style={{
+                display: 'flex',
+                margin: 'auto',
+                width: 'max-content',
+                gap: '10px'
+            }}>
+                <button className="button-32">Create tickets</button>
+                <button className="button-32">Watch my tickets</button>
+            </div>
         </>
     );
 }
